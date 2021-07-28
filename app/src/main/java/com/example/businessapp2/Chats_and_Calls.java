5bf9a8875c957fbb731f1bc422d14114dadf1aa8
Noah-Mn <bsc-com-09-17@cc.ac.mw>
2021-07-28 04:32:36 +0200

    commit

---
+++ app/src/main/java/com/example/businessapp2/Chats_and_Calls.java
@@ -0,0 +1,14 @@
+package com.example.businessapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chats_and_Calls extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chats_and_calls);
+    }
+}
--- app/src/main/java/com/example/businessapp2/Message.java
+++
@@ -1,9 +0,0 @@
-package com.example.businessapp2;
-import android.content.Context;
-import android.widget.Toast;
-
-public class Message {
-    public static void message(Context context, String message) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-    }
-}
